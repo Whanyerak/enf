9a23e132663e2f4ff487e6d5e9ec0cce15e43e9e
Robin Dupret <robin.dupret@gmail.com>
2017-06-02 14:25:26 +0200

    Ajout de validations pour les évènements

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :happen_on, presence: true
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validations" do
+    e = Event.new
+
+    e.title = 'Camp'
+    refute e.valid?
+
+    e.happen_on = 'wallah'
+    refute e.valid?
+
+    e.happen_on = DateTime.now
+    assert e.valid?
+  end
 end

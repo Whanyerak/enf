c9780a3d3dda79d05a334070d298fe43bfb60f4c
Robin Dupret <robin.dupret@gmail.com>
2017-06-07 01:16:19 +0200

    Properly kick people that are not admin from the back-end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  protected
+    def access_denied(exception)
+      redirect_to '/', alert: exception.message
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'meta#index'
 
-  devise_for :users
   ActiveAdmin.routes(self)
+  devise_for :admin_users, { class_name: 'User' }.merge(ActiveAdmin::Devise.config)
+  devise_for :users
 
   resources :topics do
   	resources :comments

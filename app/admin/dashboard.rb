f226d064aa981a1439dbb2bbe4e04d7447dd8997
Robin Dupret <robin.dupret@gmail.com>
2017-06-07 00:57:48 +0200

    Display relevant information on the dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,51 @@
 ActiveAdmin.register_page "Dashboard" do
-
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Derniers évènements" do
+          table_for Event.order(happen_on: :desc).limit(10) do
+            column("À venir") { |event| status_tag(event.upcoming?) }
+            column("Titre")   { |event| event.title }
+            column("Date")    { |event| l(event.happen_on, format: :long) }
+          end
+        end
+      end
+
+      column do
+        panel "Dernières activités du forum" do
+          last_topics   = Topic.limit(10).order(created_at: :desc)
+                            .to_a
+          last_comments = Comment.limit(10).order(created_at: :desc)
+                            .joins(:topic, :user).to_a
+          activities    = (last_topics + last_comments).first(10)
+                            .sort_by { |a| a.created_at }
+                            .reverse
+
+          table_for activities do
+            column("") do |activity|
+              if activity.kind_of?(Comment)
+                "#{activity.user.full_name} a ajouté un commentaire"
+              else
+                "#{activity.user.full_name} a créé un nouveau sujet"
+              end
+            end
+
+            column("Sujet") do |activity|
+              if activity.kind_of?(Comment)
+                link_to(activity.topic.title, activity.topic)
+              else
+                link_to(activity.title, activity)
+              end
+            end
+
+            column("Date") do |activity|
+              l(activity.created_at, format: :long)
+            end
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   validates :title, presence: true
   validates :happen_on, presence: true
+
+  def upcoming?
+    happen_on > DateTime.now
+  end
 end

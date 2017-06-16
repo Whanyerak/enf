86f1d44e5bd99fa702c8d1ae7d225d003eb946e7
Robin Dupret <robin.dupret@gmail.com>
2017-06-16 18:16:09 +0200

    Properly test the Event#upcoming? method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   def index
     events = Event.all.to_a
 
-    @upcoming_events = events.select { |e| e.happen_on > DateTime.now }
+    @upcoming_events = events.select(&:upcoming?)
     @passed_events   = events - @upcoming_events
   end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -13,4 +13,14 @@ class EventTest < ActiveSupport::TestCase
     e.happen_on = DateTime.now
     assert e.valid?
   end
+
+  test "upcoming?" do
+    e = Event.new
+
+    e.happen_on = 2.days.ago
+    refute e.upcoming?
+
+    e.happen_on = 3.days.from_now
+    assert e.upcoming?
+  end
 end
